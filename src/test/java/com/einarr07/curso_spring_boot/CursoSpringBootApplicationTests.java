5f7ae8826f8ae8fd944f5a0b7d914b2ef24e27e5
Einarr07 <aidenperse@gmail.com>
2024-10-10 12:09:09 -0500

    Creación del curso

---
+++ src/test/java/com/einarr07/curso_spring_boot/CursoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einarr07.curso_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
