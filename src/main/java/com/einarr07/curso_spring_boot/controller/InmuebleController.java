a958ec055296882d5c303c6286bccd724351fe56
Einarr07 <aidenperse@gmail.com>
2024-10-26 11:58:29 -0500

    Correccion errores tipograficos

--- src/main/java/com/einarr07/curso_spring_boot/controller/InmuebleController.java
+++ src/main/java/com/einarr07/curso_spring_boot/controller/InmuebleController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/inmueble")
+@RequestMapping("/api/inmueble")
 public class InmuebleController {
 
     @Autowired
